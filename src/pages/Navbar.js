3a403864ca2505cd98a534d156f29a5d47d39341
Kelvin Kabute <skipgh@gmail.com>
2022-02-05 14:56:35 +0000

    Fixing errors

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom'
 
 class Navbar extends React.Component {
     render() {
-        return(<React.Fragment>
+        return(<div className="header">
             <Link to="/">Home</Link>
             <Link to="/About">About Us</Link>
             <Link to="/Products">Products</Link>
             <Link to="/Cart">Cart</Link>
-        </React.Fragment>)
+        </div>)
     }
 }
 export default Navbar
